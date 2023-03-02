05c366f075207601ddde63ef04bef44282c9c5a6
joe nelson <joeynelson13@gmail.com>
2023-03-02 17:54:41 -0600

    Update main page, add tab selection clicks/switch.
    Changed main page background image, added button to main page with
    animations when hover, changed font for title section. Added company
    title to nav bar with new font. Added click events in JS file for
    nav bar to clear #content div for new page to load in. Added export
    functions in load.js file for each page to load and add html elements.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import './style.css';
 import cart_icon from './shopping-cart.png';
-import header_bg from './header-bg-4k.jpg';
-import pageLoad from './load_page.js';
+import { pageLoad, navLoad, menuPageLoad, orderPageLoad, aboutPageLoad } from './load_page.js';
 
 const cartIcon = new Image();
 cartIcon.src = cart_icon;
-pageLoad(cartIcon, header_bg);
+let navButtons = navLoad(cartIcon);
+menuPageLoad();
+
+navButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.textContent === 'Menu')
+    {
+      menuPageLoad();
+    }
+    else if (button.textContent === 'Online Order')
+    {
+      orderPageLoad();
+    }
+    else if (button.textContent === 'About Us')
+    {
+      aboutPageLoad();
+    }
+    else
+    {
+      pageLoad();
+    }
+  });
+});
--- src/load_page.js
+++ src/load_page.js
@@ -1,21 +1,53 @@
 
 
-export default function pageLoad(cartIcon, headerBg) {
+export function pageLoad() {
+  removeAllContent();
   let contentContainer = document.querySelector('#content');
+  contentContainer.classList.add('homepage-img');
+  let titleContainer = document.createElement('div');
+  let mainTitleText = document.createElement('h2');
+  let subTitleText = document.createElement('p');
+  let headerBtnContainer = document.createElement('div');
+  let orderBtn = document.createElement('button');
+  let novoBeanBtn = document.createElement('button');
+
+  titleContainer.classList.add('title-container');
+  mainTitleText.classList.add('main-title-text');
+  subTitleText.classList.add('sub-title-text');
+  headerBtnContainer.classList.add('header-btn-container');
+  orderBtn.classList.add('header-button', 'order-btn');
+  novoBeanBtn.classList.add('header-button', 'novo-btn');
+
+  mainTitleText.textContent = 'A Taste You Will Never Forget.';
+  subTitleText.textContent = 'Mundo Novo Beans ● 24 Flavors ● Specialty Drinks';
+  orderBtn.textContent = 'Order Now';
+  novoBeanBtn.textContent = 'Mundo Novo Bean';
+
+  contentContainer.appendChild(titleContainer);
+  headerBtnContainer.append(orderBtn, novoBeanBtn)
+  titleContainer.append(mainTitleText, subTitleText, headerBtnContainer);
+
+};
+
+export function navLoad(cartIcon) {
+  let body = document.querySelector('body');
   let header = document.createElement('header');
   let navbar = document.createElement('nav');
   let navLinkContainer = document.createElement('ul');
   let navLinkItemArray = [document.createElement('li'),
   document.createElement('li'),
   document.createElement('li')];
+  let navTitle = document.createElement('h1');
 
   navLinkContainer.classList.add('nav-link-container');
-
+  navTitle.classList.add('nav-title');
   cartIcon.classList.add('cart-icon');
 
-  contentContainer.appendChild(header);
+  navTitle.textContent = 'Primo Coffee'
+
+  body.prepend(header);
   header.appendChild(navbar);
-  navbar.append(navLinkContainer, cartIcon);
+  navbar.append(navLinkContainer, navTitle, cartIcon);
 
   
   navLinkItemArray.forEach(li => {
@@ -23,33 +55,36 @@ export default function pageLoad(cartIcon, headerBg) {
     navLinkContainer.appendChild(li);
   });
 
-  navLinkItemArray[0].appendChild(document.createElement('a')).textContent = 'Menu';
+  navLinkItemArray[0].textContent = 'Menu';
   navLinkItemArray[0].classList.add('menu-link');
-  navLinkItemArray[0].firstChild.setAttribute('href', '');
-  navLinkItemArray[1].appendChild(document.createElement('a')).textContent = 'Online Order';
+  navLinkItemArray[1].textContent = 'Online Order';
   navLinkItemArray[1].classList.add('order-link');
-  navLinkItemArray[1].firstChild.setAttribute('href', '');
-  navLinkItemArray[2].appendChild(document.createElement('a')).textContent = 'About Us';
+  navLinkItemArray[2].textContent = 'About Us';
   navLinkItemArray[2].classList.add('about-link');
-  navLinkItemArray[2].firstChild.setAttribute('href', '');
 
-  /////////// HEADER TITLE \\\\\\\\\\\
-  
-  let titleContainer = document.createElement('div');
-  let mainTitleText = document.createElement('h1');
-  let subTitleText = document.createElement('p');
+  let returnArray = [...navLinkItemArray, navTitle]
 
-  
+  return returnArray;
+};
 
-  titleContainer.classList.add('title-container');
-  mainTitleText.classList.add('main-title-text');
-  subTitleText.classList.add('sub-title-text');
+export function menuPageLoad() {
+  removeAllContent();
 
 
-  mainTitleText.textContent = 'One Of One Coffee';
-  subTitleText.textContent = 'Mundo Novo Beans ● 24 Flavors ● Specialty Drinks'
-
-  header.appendChild(titleContainer);
-  titleContainer.append(mainTitleText, subTitleText);
 
 };
+
+export function orderPageLoad() {
+  removeAllContent();
+};
+
+export function aboutPageLoad() {
+  removeAllContent();
+};
+
+function removeAllContent() {
+  let contentContainer = document.querySelector('#content');
+
+  contentContainer.replaceChildren();
+  contentContainer.classList.remove('homepage-img');
+}
