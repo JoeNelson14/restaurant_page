c51cbf101c1eb3a0c03740cb8c8a8de488714927
joe nelson <joeynelson13@gmail.com>
2023-03-03 18:27:04 -0600

    Add html elements to menu page.
    Added card elements with one card filled in to html elements with some
    css styles too the cards.

--- src/load_page.js
+++ src/load_page.js
@@ -70,8 +70,52 @@ export function navLoad(cartIcon) {
 export function menuPageLoad() {
   removeAllContent();
 
+  let contentContainer = document.querySelector('#content');
+  let cardContainer = document.createElement('div');
+  let menuCards = [document.createElement('div'), document.createElement('div'), document.createElement('div')];
+  let flavorDrinkList = document.createElement('ul');
+  let flavorItems = [document.createElement('li'), document.createElement('li'), document.createElement('li')];
+ 
+  cardContainer.classList.add('card-container');
+  contentContainer.appendChild(cardContainer);
+
+  menuCards.forEach(element => {
+    element.classList.add('menu-card');
+    cardContainer.appendChild(element);
+  });
 
 
+  menuCards[0].appendChild(document.createElement('h2')).classList.add('menu-title');
+  menuCards[1].appendChild(document.createElement('h2')).classList.add('flavor-title');
+  menuCards[2].appendChild(document.createElement('h2')).classList.add('special-title');
+
+  menuCards[0].firstChild.textContent = 'Menu';
+  menuCards[1].firstChild.textContent = 'Flavored Drinks';
+  menuCards[2].firstChild.textContent = 'Specialty Drinks';
+
+  menuCards[0].appendChild(document.createElement('hr'));
+  menuCards[1].appendChild(document.createElement('hr'));
+  menuCards[2].appendChild(document.createElement('hr'));
+
+  menuCards[1].appendChild(flavorDrinkList).classList.add('flavor-drink-list');
+  
+
+  flavorItems.forEach(item => {
+    item.classList.add('flavor-drink');
+    flavorDrinkList.appendChild(item);
+  });
+
+  flavorItems[0].textContent = "Carmel Brulee - hot/ice";
+  flavorItems[0].appendChild(document.createElement('p')).classList.add('item-price');
+  flavorItems[0].lastChild.textContent = '- sml ($1.99), md ($2.99), lg ($3.99)';
+
+  flavorItems[1].textContent = 'Vanilla Latte - hot/ice'
+  flavorItems[1].appendChild(document.createElement('p')).classList.add('item-price');
+  flavorItems[1].lastChild.textContent = '- sml ($1.99), md ($2.99), lg ($3.99)';
+
+  flavorItems[2].textContent = 'Mocha Latte - hot/ice'
+  flavorItems[2].appendChild(document.createElement('p')).classList.add('item-price');
+  flavorItems[2].lastChild.textContent = '- sml ($1.99), md ($2.99), lg ($3.99)';
 };
 
 export function orderPageLoad() {
